61e2e3a64ca45f3865765013ec87a522fbd82df6
nmustakin <nmust004@ucr.edu>
2022-03-04 19:12:56 -0800

    add code for load/store

--- Pass/Transforms/Liveness/LivenessCheck.cpp
+++ Pass/Transforms/Liveness/LivenessCheck.cpp
@@ -4,7 +4,8 @@
 #include "llvm/IR/PassManager.h"
 
 #include "llvm/Support/raw_ostream.h"
-
+#include "llvm/IR/CFG.h"
+#include <set>
 #include <string>
 #include <unordered_map>
 #include <sstream> 
@@ -15,6 +16,7 @@ using namespace llvm;
 
 namespace {
 
+typedef unordered_map<BasicBlock*, set<string> > blockset; 
 
 // This method implements what the pass does
 void visitor(Function &F){
@@ -27,25 +29,42 @@ void visitor(Function &F){
 	    // Comment this line
         //if (F.getName() != func_name) return;
 	
-//	unsigned int loc = 0; 
+    blockset UEVar; 
+    blockset VarKill;
+    blockset LiveOut; 
 
-        unsigned int blockID = 0; 
-        for (auto& basic_block : F)
-        {
-            for (auto& inst : basic_block)
-            {
+    unordered_map<Value*, string> variables;     
+    
+    for (auto& basic_block : F){
+
+        VarKill[&basic_block] = set<string>();
+        UEVar[&basic_block] = set<string>(); 
+            
+        for (auto& inst : basic_block){
+
                 //errs() << inst << "\n";
-                if(inst.getOpcode() == Instruction::Load){
+                
+            if(inst.getOpcode() == Instruction::Load){
                     // errs() << "This is Load"<<"\n";
 
-                }
-                if(inst.getOpcode() == Instruction::Store){
+                variables[&inst] = inst.getOperand(0)->getName().str();                 
+            }
+                
+            if(inst.getOpcode() == Instruction::Store){
                     //errs() << "This is Store"<<"\n";
-
-
+               
+               /* 
+                string operand1 = inst.getOperand(0)->getName().str();
+                if(variables.find(operand1) != variables.end()){
+                    UEVar[&basic_block].insert(variables[operand1]);
                 }
-                if (inst.isBinaryOp())
-                {
+                */
+                VarKill[&basic_block].insert(inst.getOperand(1)->getName().str()); 
+            }
+                
+            if (inst.isBinaryOp())
+                
+            {
                     /*
                     errs() << "Op Code:" << inst.getOpcodeName()<<"\n";
                     if(inst.getOpcode() == Instruction::Add){
@@ -62,19 +81,25 @@ void visitor(Function &F){
 
                     // see other classes, Instruction::Sub, Instruction::UDiv, Instruction::SDiv
                     // errs() << "Operand(0)" << (*inst.getOperand(0))<<"\n";
-                    auto* ptr = dyn_cast<User>(&inst);
+                    
+                auto* ptr = dyn_cast<User>(&inst);
 		    		//errs() << "\t" << *ptr << "\n";
-                    for (auto it = ptr->op_begin(); it != ptr->op_end(); ++it) {
+                    
+                for (auto it = ptr->op_begin(); it != ptr->op_end(); ++it) {
                         //errs() << "\t" <<  *(*it) << "\n";
                         // if ((*it)->hasName()) 
 			    		// errs() << (*it)->getName() << "\n";
                         
-                    }
                     
-                } // end if
-            } // end for inst
+                }
+                    
+                
+            } // end if
+            
+        } // end for inst
             //printHashTable(ht, ++blockID); 
-        } // end for block
+        
+    } // end for block
         
 }
 
