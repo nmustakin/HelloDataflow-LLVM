c9ce325accc718aa3cfb35aeb9473e84d226006b
Jannat Ara Meem <jannatarameem@gpvpn-general-172-18-10-138.net.ucr.edu>
2022-03-07 01:39:42 -0800

    UEVAR, VARKILL works, LIVEOUT doesn't work

--- Pass/Transforms/Liveness/LivenessCheck.cpp
+++ Pass/Transforms/Liveness/LivenessCheck.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <unordered_map>
 #include <sstream> 
+#include <queue>
 
 using namespace std;
 
@@ -16,11 +17,24 @@ using namespace llvm;
 
 namespace {
 
+void printHashTable(set<string>  s){
+
+    set<string>::iterator itr;
+    
+  // Displaying set elements
+    for (itr = s.begin(); itr != s.end(); itr++) 
+    {
+        errs() << *itr << " ";
+    }
+    errs() <<"\n";
+        
+}
+
+
 typedef unordered_map<BasicBlock*, set<string> > blockset; 
 
 // This method implements what the pass does
 void visitor(Function &F){
-
     // Here goes what you want to do with a pass
     
 	string func_name = "main";
@@ -33,74 +47,142 @@ void visitor(Function &F){
     blockset VarKill;
     blockset LiveOut; 
 
-    unordered_map<Value*, string> variables;     
+    unordered_map<Value*, string> variables;    
+    queue<BasicBlock*> worklist = queue<BasicBlock*>();
     
     for (auto& basic_block : F){
-
+        
         VarKill[&basic_block] = set<string>();
         UEVar[&basic_block] = set<string>(); 
+        LiveOut[&basic_block] = set<string>();
+
+        worklist.push(&basic_block);
+
+        StringRef bbName(basic_block.getName());
+        
+        errs() << bbName  << ":\n";
             
         for (auto& inst : basic_block){
 
-                //errs() << inst << "\n";
-                
             if(inst.getOpcode() == Instruction::Load){
                     // errs() << "This is Load"<<"\n";
 
                 variables[&inst] = inst.getOperand(0)->getName().str();                 
             }
-                
-            if(inst.getOpcode() == Instruction::Store){
-                    //errs() << "This is Store"<<"\n";
-               
-               /* 
-                string operand1 = inst.getOperand(0)->getName().str();
-                if(variables.find(operand1) != variables.end()){
-                    UEVar[&basic_block].insert(variables[operand1]);
-                }
-                */
-                VarKill[&basic_block].insert(inst.getOperand(1)->getName().str()); 
-            }
-                
-            if (inst.isBinaryOp())
-                
-            {
-                    /*
-                    errs() << "Op Code:" << inst.getOpcodeName()<<"\n";
-                    if(inst.getOpcode() == Instruction::Add){
-                        errs() << "This is Addition"<<"\n";
-                    }
-                    if(inst.getOpcode() == Instruction::Load){
-                        errs() << "This is Load"<<"\n";
-                    }
-                    if(inst.getOpcode() == Instruction::Mul){
-                        errs() << "This is Multiplication"<<"\n";
-                    }
-                    */
-                    
 
-                    // see other classes, Instruction::Sub, Instruction::UDiv, Instruction::SDiv
-                    // errs() << "Operand(0)" << (*inst.getOperand(0))<<"\n";
-                    
+            if (inst.isBinaryOp())
+            {   
                 auto* ptr = dyn_cast<User>(&inst);
-		    		//errs() << "\t" << *ptr << "\n";
-                    
+		    	
                 for (auto it = ptr->op_begin(); it != ptr->op_end(); ++it) {
-                        //errs() << "\t" <<  *(*it) << "\n";
-                        // if ((*it)->hasName()) 
-			    		// errs() << (*it)->getName() << "\n";
-                        
                     
                 }
+  
+            } 
+            if(inst.getOpcode() == Instruction::Store || ((StringRef)inst.getOpcodeName())== "icmp"){
+
+                int pick=1;
+                StringRef compName(inst.getOpcodeName());
+                if(compName == "icmp"){
+                    //errs() << "Op Code:" << inst.getOpcodeName()<<"\n";
+                    pick = 2;
+                }
                     
-                
+                for(int p =0; p<pick;p++){
+                        
+                    string operand = inst.getOperand(p)->getName().str();
+                    //errs() << "Op Code:" << inst.getOpcodeName()<<  "\n";
+                    if (llvm::ConstantInt* CI = dyn_cast<llvm::ConstantInt>(inst.getOperand(p))) { 
+                        //errs() << to_string(CI->getSExtValue()) << "\n";
+                    }
+                    
+                    else if(llvm::Instruction* instr = dyn_cast<Instruction>(inst.getOperand(p))){
+                        //errs() << *instr << "\n";
+                        if(instr->getOpcode() == Instruction::Load){
+                            operand=(*(instr->getOperand(0))).getName().str();
+                            //errs() << "\t Load: " << operand << "\n";
+                            if(VarKill[&basic_block].find(operand) == VarKill[&basic_block].end()){
+                                
+                                UEVar[&basic_block].insert(operand);
+                            }
+                        }
+                        else if(instr->isBinaryOp()){
+                        //auto* ptr = dyn_cast<User>(instr);
+                        //errs() << "\tBinary" << *instr << "\n";
+
+                            int i=0;
+                            for (i=0; i<2; i++){
+                                llvm::User* x = dyn_cast<User>(instr->getOperand(i));
+                                if (llvm::ConstantInt* CI = dyn_cast<llvm::ConstantInt>(x)) {
+                                
+                                }
+                                else{
+                                    for (auto err = x->op_begin(); err != x->op_end(); ++err){
+                                        if ((*err)->hasName()) {
+                                            operand = ((*err)->getName()).str();
+                                            //errs() << "\t Binary: " << operand << "\n";
+                                            if(VarKill[&basic_block].find(operand) == VarKill[&basic_block].end()){
+                                                
+                                                UEVar[&basic_block].insert(operand);
+                                            }
+                                        
+                                        }
+                                    }
+                                }  
+                            }
+                        }
+                    
+                    }
+                }
+
+                if(pick ==1 ){
+                    string operand2= inst.getOperand(1)->getName().str();
+                    VarKill[&basic_block].insert(operand2);
+                }
             } // end if
             
         } // end for inst
-            //printHashTable(ht, ++blockID); 
+        
+        errs() << "UEVAR: ";
+        printHashTable(UEVar[&basic_block]); 
+        errs() << "VARKILL: ";
+        printHashTable(VarKill[&basic_block]);
+        errs() <<"\n";
         
     } // end for block
+
+    while(!worklist.empty()){
+        BasicBlock* top = worklist.front();
+        worklist.pop();
+        set<string> newLiveOut= set<string>();
+        for (BasicBlock *Succ : successors(top)) {
+            
+            set<string> dest1 = set<string>();
+            set<string>::iterator it; 
+            set_difference(LiveOut[Succ].begin(), LiveOut[Succ].end(), VarKill[Succ].begin(), VarKill[Succ].end(), dest1.begin());
+            //set_union(dest1.begin(), it, UEVar[Succ].begin(), UEVar[Succ].end(), dest1.begin());
+            //set_union(newLiveOut.begin(), newLiveOut.end(), dest1.begin(), dest1.end(), back_inserter(newLiveOut));
+        }
+        /*
+        if(newLiveOut!=LiveOut[top]){
+            for (BasicBlock *Pred : predecessors(top)){
+                worklist.push(Pred);
+            }
+        }*/
+    }
+
+/*
+    errs() << "LIVEOUT:\n";
+    for (auto& basic_block : LiveOut)
+    {
+        StringRef bbName(basic_block->getName());
         
+        errs() << "Basic Block: "<< bbName  << ":\n";
+
+        printHashTable(LiveOut[basic_block]);
+    }
+    */
+    
 }
 
 
